98cafa83f50257753fa0d706969dba339ee91d93
Prestonast <82960061+Prestonast@users.noreply.github.com>
2021-07-26 17:13:50 -0500

    Add files via upload

---
+++ population.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int s,e,y;
+    // TODO: Prompt for start size
+    do
+    {
+        s = get_int ("Start Size: ");
+    }
+    while (s < 9);
+    // TODO: Prompt for end size
+    do
+    {
+        e = get_int ("End Size:");
+    }
+    while (e < s);
+    // TODO: Calculate number of years until we reach threshold
+    for (y = 0; s < e; y++)
+    {
+        s = s + (s / 3) - (s / 4);
+    }
+    // TODO: Print number of years
+    printf ("Years: %i", y);
+}
